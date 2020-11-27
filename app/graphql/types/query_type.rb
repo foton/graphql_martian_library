c6c5e8f5a9a59a283733b0f81459b8f0bab39498
foton <foton@centrum.cz>
2020-11-27 16:18:04 +0100

    Added graphql ItemType

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -3,11 +3,13 @@ module Types
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
 
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    field :items,
+      [Types::ItemType], # arrray => returns array of Type
+      null: false,
+      description: "return list of items in the martian library"
+
+    def items
+      Item.all
     end
   end
 end
